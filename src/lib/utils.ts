144dff26ad03ca8c7fd407b1ae9bb5830a1d6344
achou11 <andrewchou@fastmail.com>
2022-02-25 15:43:40 -0500

    Remove unused getTilesetFormat util

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,28 +1,8 @@
 import { createHash, randomBytes } from 'crypto'
-import path from 'path'
-import { URL } from 'url'
 import { getTileBBox } from '@mapbox/whoots-js'
 import base32 from 'base32.js'
 
 import { TileJSON } from './tilejson'
-
-export function getTilesetFormat(tileset: TileJSON): TileJSON['format'] {
-  if (tileset.format) return tileset.format
-  // attempt to parse format from tile url
-  const url = new URL(tileset.tiles[0])
-  const ext = path.extname(url.pathname).slice(1)
-  // See https://docs.mapbox.com/api/maps/#raster-tiles for extensions
-  // TODO: Common options for other tile services?
-  if (ext.match(/^png\d{0,3}$/)) {
-    return 'png'
-  } else if (ext.match(/^je?pg\d{0,2}$/)) {
-    return 'jpg'
-  } else if (ext === 'webp') {
-    return 'webp'
-  } else {
-    return 'png'
-  }
-}
 
 // Not cryptographically secure, but sha1 results in shorter / more manageable
 // ids for filenames and in the URL, should be fine for our use-case
