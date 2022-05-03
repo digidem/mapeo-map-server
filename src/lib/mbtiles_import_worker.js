1fc5ab3685fc63bdfdd8321d064555b3c7d1b23d
Andrew Chou <andrewchou@fastmail.com>
2022-05-03 17:13:42 -0400

    fix: fix sqlite error when running dev server

--- src/lib/mbtiles_import_worker.js
+++ src/lib/mbtiles_import_worker.js
@@ -38,42 +38,75 @@ const { extractMBTilesMetadata } = require('./mbtiles')
  * @property {'importTerminate'} type
  */
 
+const queries = {
+  /**
+   *
+   * @param {{ id: string, zoomLevel: string, boundingBox: string, name: string, styleId: string }} params
+   * @returns {Database.RunResult}
+   */
+  upsertOfflineArea: (params) =>
+    db
+      .prepare(
+        'INSERT INTO OfflineArea (id, zoomLevel, boundingBox, name, styleId) ' +
+          'VALUES (:id, :zoomLevel, :boundingBox, :name, :styleId) ' +
+          'ON CONFLICT (id) DO UPDATE SET ' +
+          'zoomLevel = excluded.zoomLevel, boundingBox = excluded.boundingBox, name = excluded.name, styleId = excluded.styleId'
+      )
+      .run(params),
+  /**
+   *
+   * @param {{ id: string, totalResources: number, areaId: string, tilesetId?: string }} params
+   * @returns {Database.RunResult}
+   */
+  insertImport: (params) =>
+    db
+      .prepare(
+        'INSERT INTO Import (id, totalResources, areaId, tilesetId, importedResources, isComplete, importType) ' +
+          "VALUES (:id, :totalResources, :areaId, :tilesetId, 0, 0, 'tileset')"
+      )
+      .run(params),
+  /**
+   *
+   * @param {{ id: string, importedResources: number, isComplete: number }} params
+   * @returns {Database.RunResult}
+   */
+  updateImport: (params) =>
+    db
+      .prepare(
+        'UPDATE Import SET importedResources = :importedResources, isComplete = :isComplete, finished = CURRENT_TIMESTAMP WHERE id = :id'
+      )
+      .run(params),
+  /**
+   *
+   * @param {{ data: Buffer, tileHash: string, tilesetId: string }} params
+   * @returns {Database.RunResult}
+   */
+  upsertTileData: (params) =>
+    db
+      .prepare(
+        'INSERT INTO TileData (tileHash, data, tilesetId) VALUES (:tileHash, :data, :tilesetId) ' +
+          'ON CONFLICT (tileHash, tilesetId) DO UPDATE SET data = excluded.data'
+      )
+      .run(params),
+  /**
+   *
+   * @param {{ quadKey: string, tileHash: string, tilesetId: string }} params
+   * @returns {Database.RunResult}
+   */
+  upsertTile: (params) =>
+    db
+      .prepare(
+        'INSERT INTO Tile (quadKey, tileHash, tilesetId) VALUES (:quadKey, :tileHash, :tilesetId) ' +
+          'ON CONFLICT (quadkey, tilesetId) DO UPDATE SET tilehash = excluded.tileHash'
+      )
+      .run(params),
+}
+
 /** @type {Set<string>} */
 const subscriptions = new Set()
 
 /** @type {Database} */
 const db = new Database(workerData.dbPath)
-
-/** @type {Statement<{ id: string, zoomLevel: string, boundingBox: string, name: string, styleId: string }>} */
-const upsertOfflineArea = db.prepare(
-  'INSERT INTO OfflineArea (id, zoomLevel, boundingBox, name, styleId) ' +
-    'VALUES (:id, :zoomLevel, :boundingBox, :name, :styleId) ' +
-    'ON CONFLICT (id) DO UPDATE SET ' +
-    'zoomLevel = excluded.zoomLevel, boundingBox = excluded.boundingBox, name = excluded.name, styleId = excluded.styleId'
-)
-
-/** @type {Statement<{ id: string, totalResources: number, areaId: string, tilesetId?: string }>} */
-const insertImport = db.prepare(
-  'INSERT INTO Import (id, totalResources, areaId, tilesetId, importedResources, isComplete, importType) ' +
-    "VALUES (:id, :totalResources, :areaId, :tilesetId, 0, 0, 'tileset')"
-)
-
-/** @type {Statement<{ id: string, importedResources: number, isComplete: number }>} */
-const updateImport = db.prepare(
-  'UPDATE Import SET importedResources = :importedResources, isComplete = :isComplete, finished = CURRENT_TIMESTAMP WHERE id = :id'
-)
-
-/** @type {Statement<{ data: Buffer, tileHash: string, tilesetId: string }>} */
-const upsertTileData = db.prepare(
-  'INSERT INTO TileData (tileHash, data, tilesetId) VALUES (:tileHash, :data, :tilesetId) ' +
-    'ON CONFLICT (tileHash, tilesetId) DO UPDATE SET data = excluded.data'
-)
-
-/** @type {Statement<{ quadKey: string, tileHash: string, tilesetId: string }>} */
-const upsertTile = db.prepare(
-  'INSERT INTO Tile (quadKey, tileHash, tilesetId) VALUES (:quadKey, :tileHash, :tilesetId) ' +
-    'ON CONFLICT (quadkey, tilesetId) DO UPDATE SET tilehash = excluded.tileHash'
-)
 
 if (!parentPort) throw new Error('No parent port found')
 
@@ -130,7 +163,7 @@ function importMbTiles({ importId, mbTilesDbPath, tilesetId, styleId }) {
 
   const areaId = encodeBase32(hash(`area:${tilesetId}`))
 
-  upsertOfflineArea.run({
+  queries.upsertOfflineArea({
     id: areaId,
     boundingBox: JSON.stringify(mbTilesMetadata.bounds),
     name: mbTilesMetadata.name,
@@ -141,7 +174,7 @@ function importMbTiles({ importId, mbTilesDbPath, tilesetId, styleId }) {
     styleId,
   })
 
-  insertImport.run({
+  queries.insertImport({
     id: importId,
     totalResources: totalBytesToImport,
     tilesetId,
@@ -156,13 +189,13 @@ function importMbTiles({ importId, mbTilesDbPath, tilesetId, styleId }) {
     const tileHash = hash(data).toString('hex')
 
     const tilesImportTransaction = db.transaction(() => {
-      upsertTileData.run({
+      queries.upsertTileData({
         tileHash,
         data,
         tilesetId,
       })
 
-      upsertTile.run({
+      queries.upsertTile({
         quadKey,
         tileHash,
         tilesetId,
@@ -170,7 +203,7 @@ function importMbTiles({ importId, mbTilesDbPath, tilesetId, styleId }) {
 
       bytesSoFar += data.byteLength
 
-      updateImport.run({
+      queries.updateImport({
         id: importId,
         importedResources: bytesSoFar,
         isComplete: bytesSoFar === totalBytesToImport ? 1 : 0,
