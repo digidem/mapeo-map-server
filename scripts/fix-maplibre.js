f7663ed268e439925fc580daae9cc5d4ce2760d9
Andrew Chou <andrewchou@fastmail.com>
2022-05-04 18:10:26 -0400

    chore: fix postinstall step
    still needs to fix issue with patch-package when installed via some
    other project

--- scripts/fix-maplibre.js
+++ scripts/fix-maplibre.js
@@ -4,13 +4,17 @@
  */
 const fs = require('fs')
 const path = require('path')
+const process = require('process')
 
 const PACKAGE_JSON_PATH = path.join(
-  require.resolve('@maplibre/maplibre-gl-style-spec'),
-  '../../package.json'
+  // When executing the "postinstall" script, the "process.cwd" equals
+  // the package directory, not the parent project where the package is installed.
+  // NPM stores the parent project directory in the "INIT_CWD" env variable.
+  process.env.INIT_CWD,
+  'node_modules/@maplibre/maplibre-gl-style-spec/package.json'
 )
 
-const file = fs.readFileSync(PACKAGE_JSON_PATH, { encoding: 'utf8' })
+const file = fs.readFileSync(PACKAGE_JSON_PATH, 'utf8')
 
 const parsed = JSON.parse(file)
 
@@ -20,5 +24,5 @@ fs.writeFileSync(
 )
 
 console.log(
-  'Applied hacky patch to @maplibre/maplibre-gl-style-spec/package.json'
+  'Applied non-traditional patch to @maplibre/maplibre-gl-style-spec/package.json'
 )
