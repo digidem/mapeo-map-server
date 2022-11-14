3ce4e4e8d310ba5c1f1b708598ead09dd235e674
Andrew Chou <andrewchou@fastmail.com>
2022-11-14 13:49:46 -0500

    feat: include style id in response for tileset import endpoint (#82)

--- src/routes/tilesets.ts
+++ src/routes/tilesets.ts
@@ -45,6 +45,7 @@ const ImportMBTilesRequestBodySchema = T.Object({
 
 const ImportMBTilesResponseBodySchema = T.Object({
   import: T.Object({ id: T.String() }),
+  style: T.Union([T.Object({ id: T.String() }), T.Null()]),
   tileset: TileJSONSchema,
 })
 
